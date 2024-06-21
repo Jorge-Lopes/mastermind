c0e2d3511fa6442b5c46bcd97af87b02a94a22f2
JorgeLopes-BytePitch <jorge.lopes@bytepitch.com>
2024-06-21 18:53:52 +0100

    chore(proposal): add mastermind proposal

---
+++ contract/src/proposal.js
@@ -0,0 +1,69 @@
+import { E } from '@endo/eventual-send';
+
+console.warn('start proposal module evaluating');
+
+export const startMastermind = async permittedPowers => {
+  console.error('startMastermind...');
+
+  const {
+    consume: { board, chainStorage, chainTimerService, startUpgradable },
+    installation: {
+      consume: { mastermind: mastermindInstallationP },
+    },
+    instance: {
+      produce: { mastermind: mastermindInstance },
+    },
+  } = permittedPowers;
+
+  const marshaller = await E(board).getPublishingMarshaller();
+  const storageNode = await E(chainStorage).makeChildNode('mastermind');
+
+  const privateArgs = harden({ marshaller, storageNode, chainTimerService });
+  const installation = await mastermindInstallationP;
+
+  const { instance } = await E(startUpgradable)({
+    installation,
+    undefined,
+    undefined,
+    privateArgs,
+    label: 'Mastermind',
+  });
+
+  console.log('CoreEval script: started contract', instance);
+
+  mastermindInstance.reset();
+  mastermindInstance.resolve(instance);
+
+  console.log('mastermind (re)started');
+};
+
+const mastermindManifest = {
+  [startMastermind.name]: {
+    consume: {
+      board: true,
+      chainStorage: true,
+      chainTimerService: true,
+      startUpgradable: true,
+    },
+    installation: {
+      consume: {
+        mastermind: true,
+      },
+    },
+    instance: {
+      produce: {
+        mastermind: true,
+      },
+    },
+  },
+};
+harden(mastermindManifest);
+
+export const getManifestForMastermind = ({ restoreRef }, { contractRef }) => {
+  return harden({
+    manifest: mastermindManifest,
+    installations: {
+      mastermind: restoreRef(contractRef),
+    },
+  });
+};
