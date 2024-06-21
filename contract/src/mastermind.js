558228c7eeca9432819590270cc545e74a7b7cdf
JorgeLopes-BytePitch <jorge.lopes@bytepitch.com>
2024-06-21 18:49:58 +0100

    feat(contract): add mastermind contract and support functions

---
+++ contract/src/mastermind.js
@@ -0,0 +1,193 @@
+// @ts-check
+import { M, prepareExoClassKit } from '@agoric/vat-data';
+import { makeRecorderTopic } from '@agoric/zoe/src/contractSupport/index.js';
+import { E } from '@endo/eventual-send';
+import { generateSecretCode, makeAssertions } from './utils.js';
+
+/**
+ * @returns
+ */
+export const preparePlayerKit = () => {
+  const makePlayerKit = game => {
+    const { invitationMakers, publicTopics } = game;
+
+    const playerKit = harden({
+      publicTopics,
+      invitationMakers,
+    });
+
+    return playerKit;
+  };
+  return makePlayerKit;
+};
+harden(preparePlayerKit);
+
+/**
+ *
+ * @param {*} zcf
+ * @param {import('@agoric/vat-data').Baggage} baggage
+ * @param {import('@agoric/zoe/src/contractSupport/recorder.js').MakeRecorderKit} makeRecorderKit
+ * @returns
+ */
+export const prepareGame = (zcf, baggage, makeRecorderKit) => {
+  const makePlayerKit = preparePlayerKit();
+  const assertions = makeAssertions();
+
+  const phase = {
+    ACTIVE: 'active',
+    COMPLETED: 'completed',
+    FAILED: 'failed',
+  };
+
+  const interfaceGuardKit = {
+    helper: M.interface('helper', {}, { sloppy: true }),
+    publicTopics: M.interface('publicTopics', {}, { sloppy: true }),
+    invitationMakers: M.interface('invitationMakers', {}, { sloppy: true }),
+    self: M.interface('self', {}, { sloppy: true }),
+  };
+
+  const init = (gameNode, seed) => {
+    const secretCode = generateSecretCode(seed);
+    assertions.assertCode(secretCode);
+
+    const mastermindState = harden({
+      guessList: [],
+      feedbackList: [],
+      attemptsLeft: 10,
+      phase: phase.ACTIVE,
+    });
+
+    const recorderKit = makeRecorderKit(gameNode);
+    E(recorderKit.recorder).write(mastermindState);
+
+    return harden({ recorderKit, secretCode, mastermindState });
+  };
+
+  const makeGame = prepareExoClassKit(
+    baggage,
+    'mastermind game',
+    interfaceGuardKit,
+    init,
+    {
+      helper: {
+        updateMastermindState(updatedState) {
+          const { recorderKit, mastermindState } = this.state;
+          const { guessList, feedbackList, attemptsLeft } = mastermindState;
+          const { guessCode, feedback } = updatedState;
+
+          const newPhase =
+            feedback.correctPositionCount === 4
+              ? phase.COMPLETED
+              : attemptsLeft > 1
+              ? phase.ACTIVE
+              : phase.FAILED;
+
+          const newMastermindState = harden({
+            guessList: [...guessList, guessCode],
+            feedbackList: [...feedbackList, feedback],
+            attemptsLeft: attemptsLeft - 1,
+            phase: newPhase,
+          });
+
+          // @ts-expect-error
+          this.state.mastermindState = newMastermindState;
+          E(recorderKit.recorder).write(this.state.mastermindState);
+        },
+        evaluateGuess(guessCode) {
+          const secretCode = this.state.secretCode;
+          const digitCountSecretCode = new Map();
+          const digitCountGuessCode = new Map();
+          let correctPositionCount = 0;
+          let correctValueCount = 0;
+
+          for (let i = 0; i < secretCode.length; i++) {
+            if (secretCode[i] === guessCode[i]) {
+              correctPositionCount++;
+            }
+          }
+
+          for (let i = 0; i < secretCode.length; i++) {
+            digitCountSecretCode.set(
+              secretCode[i],
+              (digitCountSecretCode.get(secretCode[i]) || 0) + 1,
+            );
+            digitCountGuessCode.set(
+              guessCode[i],
+              (digitCountGuessCode.get(guessCode[i]) || 0) + 1,
+            );
+          }
+
+          for (const [digit, countSecret] of digitCountSecretCode) {
+            const countGuess = digitCountGuessCode.get(digit) || 0;
+            correctValueCount += Math.min(countSecret, countGuess);
+          }
+          correctValueCount -= correctPositionCount;
+
+          return { correctPositionCount, correctValueCount };
+        },
+
+        makeGuessHandler(userSeat, offerArgs) {
+          const {
+            facets: { helper },
+            state: { mastermindState },
+          } = this;
+
+          if (mastermindState.phase !== phase.ACTIVE) {
+            E(userSeat).exit();
+            return 'Game finished.';
+          }
+
+          const { guessCode } = offerArgs;
+          assertions.assertCode(guessCode);
+          const feedback = helper.evaluateGuess(guessCode);
+
+          const stateUpdate = {
+            guessCode,
+            feedback,
+          };
+          helper.updateMastermindState(stateUpdate);
+
+          switch (this.state.mastermindState.phase) {
+            case phase.COMPLETED:
+              return 'Congratulations, you won the game.';
+            case phase.FAILED:
+              return 'Sorry, you lost the game.';
+            default:
+              return 'Try again.';
+          }
+        },
+      },
+      publicTopics: {
+        getMastermindState() {
+          const { recorderKit } = this.state;
+          return makeRecorderTopic('Mastermind state', harden(recorderKit));
+        },
+        // ToDo: remove this method. Just for testing purpose
+        getMastermindSecretCode() {
+          return harden(this.state.secretCode);
+        },
+      },
+      invitationMakers: {
+        makeGuessInvitation() {
+          return zcf.makeInvitation(
+            (seat, offerArgs) =>
+              this.facets.helper.makeGuessHandler(seat, offerArgs),
+            'make a guess offer',
+          );
+        },
+      },
+      self: {
+        initPlayerKit() {
+          const {
+            facets: { publicTopics, invitationMakers },
+          } = this;
+          const playerKit = makePlayerKit({ publicTopics, invitationMakers });
+          return playerKit;
+        },
+      },
+    },
+  );
+
+  return harden(makeGame);
+};
+harden(prepareGame);
---
+++ contract/src/utils.js
@@ -0,0 +1,66 @@
+// @ts-check
+import MersenneTwister from 'mersenne-twister';
+
+let debugInstance = 1;
+
+/**
+ * @param {string} name
+ * @param {boolean | 'verbose'} enable
+ */
+export const makeTracer = (name, enable = true) => {
+  debugInstance += 1;
+  let debugCount = 1;
+  const key = `----- ${name}.${debugInstance} `;
+  // the cases below define a named variable to provide better debug info
+  switch (enable) {
+    case false: {
+      const logDisabled = (..._args) => {};
+      return logDisabled;
+    }
+    case 'verbose': {
+      const infoTick = (optLog, ...args) => {
+        if (optLog.log) {
+          console.info(key, (debugCount += 1), ...args);
+        } else {
+          console.info(key, (debugCount += 1), optLog, ...args);
+        }
+      };
+      return infoTick;
+    }
+    default: {
+      const debugTick = (optLog, ...args) => {
+        if (optLog.log) {
+          optLog.log(key, (debugCount += 1), ...args);
+        } else {
+          console.info(key, (debugCount += 1), optLog, ...args);
+        }
+      };
+      return debugTick;
+    }
+  }
+};
+harden(makeTracer);
+
+export const generateSecretCode = seed => {
+  const generator = new MersenneTwister(seed);
+  const options = 6;
+  const secretCode = [];
+  for (let i = 0; i < 4; i++) {
+    secretCode.push(Math.floor(options * generator.random()));
+  }
+  return secretCode;
+};
+harden(generateSecretCode);
+
+export const makeAssertions = () => {
+  const assertCode = code => {
+    assert(code.length === 4, `Invalid code length: ${code.length}`);
+
+    for (const element of code) {
+      assert(element >= 0 && element < 6, `Invalid code value: ${element}`);
+    }
+  };
+
+  return { assertCode };
+};
+harden(makeAssertions);
